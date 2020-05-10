ea2e0544a8eca69a608b6cc15f13115216205f9c
나영윤 <fightnyy@naver.com>
2020-05-11 00:28:36 +0900

    2번째 작업 카메라 연동 및 파이어베이스 storage 저장

--- Sns_example/app/src/main/java/org/techtown/sns_example/MainActivity.java
+++ Sns_example/app/src/main/java/org/techtown/sns_example/MainActivity.java
@@ -1,19 +1,16 @@
 package org.techtown.sns_example;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -33,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
             MystartActivity(LoginActivity.class);
 
         } else {
+            MystartActivity(CameraActivity.class);
+
             FirebaseFirestore db = FirebaseFirestore.getInstance();
             DocumentReference docRef = db.collection("users").document(user.getUid());
             docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
